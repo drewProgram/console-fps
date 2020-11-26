beeb3ed46ba7c899f96f8d91381037f2d87351c2
Andrew Filgueiras <andrew.lascasas@gmail.com>
2020-11-25 21:42:45 -0300

    feat: added movement to character

--- ConFPS/ConFPS.cpp
+++ ConFPS/ConFPS.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 
 #include <Windows.h>
@@ -6,20 +7,20 @@ using namespace std;
 int nScreenWidth = 120;
 int nScreenHeight = 40;
 
-float fPlayerX = 0.0f;
-float fPlayerY = 0.0f;
+float fPlayerX = 8.0f;
+float fPlayerY = 8.0f;
 float fPlayerA = 0.0f;
 
 int nMapHeight = 16;
 int nMapWidth = 16;
 
 float fFOV = 3.14159 / 4.0;
-float fDepth = 16.0;
+float fDepth = 16.0f;
 
 int main()
 {
 	// create screen buffer
-	wchar_t* screen = new wchar_t[nScreenHeight * nScreenWidth];
+	wchar_t *screen = new wchar_t[nScreenHeight * nScreenWidth];
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0,
 		NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
@@ -27,26 +28,55 @@ int main()
 
 	wstring map;
 
-	map += L"###############";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"#.............#";
-	map += L"###############";
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
+	auto tp1 = chrono::system_clock::now();
+	auto tp2 = chrono::system_clock::now();
 
 	// game loop
 	while (1)
 	{
+		tp2 = chrono::system_clock::now();
+		chrono::duration<float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float fElapsedTime = elapsedTime.count();
+
+
+		// controls
+		// handle CCW rotation
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
+			fPlayerA -= (0.3f) * fElapsedTime;
+
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
+			fPlayerA += (0.3f) * fElapsedTime;
+
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
+		{
+			fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerX += cosf(fPlayerA) * 5.0f * fElapsedTime;
+		}
+
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)
+		{
+			fPlayerX -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+		}
+
 		for (int x = 0; x < nScreenWidth; x++)
 		{
 			//for each column, calculate the projected ray angle into world space
@@ -57,7 +87,7 @@ int main()
 
 			float fEyeX = sinf(fRayAngle); // unit vector for ray in player space
 			float fEyeY = cosf(fRayAngle);
-
+			
 			while (!bHitWall && fDistanceToWall < fDepth)
 			{
 				fDistanceToWall += 0.1f;
@@ -68,7 +98,38 @@ int main()
 				// test if ray is out of bounds
 				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight) 
 				{
+					bHitWall = true;
+					fDistanceToWall = fDepth;
+				}
+				else
+				{
+					// ray is inbounds so test to see if ray cell is a wall block
+					if (map[nTestY * nMapWidth + nTestX] == '#')
+					{
+						bHitWall = true;
+					}
 
+					// calculate distance to ceiling and floor
+					int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+					int nFloor = nScreenHeight - nCeiling;
+
+					short nShade = ' ';
+
+					if (fDistanceToWall <= fDepth / 4.0f)		nShade = 0x2588;	// very close
+					else if (fDistanceToWall < fDepth / 3.0f)	nShade = 0x2593;
+					else if (fDistanceToWall < fDepth / 2.0f)	nShade = 0x2592;
+					else if (fDistanceToWall < fDepth)			nShade = 0x2591;
+					else										nShade = ' ';		// too far away
+
+					for (int y = 0; y < nScreenHeight; y++)
+					{
+						if (y < nCeiling)
+							screen[y * nScreenWidth + x] = ' ';
+						else if(y > nCeiling && y <= nFloor)
+							screen[y * nScreenWidth + x] = nShade;
+						else
+							screen[y * nScreenWidth + x] = ' ';
+					}
 				}
 			}
 		}
